b78c98e797444894074fabe6f55c88c5d30eeeca
Luch-z <ricardopacja@gmail.com>
2025-01-21 09:29:02 -0300

    File Renamed to UIElements

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button({children, className}) {
-    return (
-        <button className={`bg-[#4CAF4F] py-1 px-5 text-white rounded-[3px] ${className}`}>
-            {children}
-        </button>
-    )
-}
-
-export default Button;
